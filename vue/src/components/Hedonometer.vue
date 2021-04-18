7621de58cb565e1e5cfb03359e34a9894da54781
tiffylou <tiffany6872@gmail.com>
2021-04-18 18:56:02 -0400

    hedonometer data

--- vue/src/components/Hedonometer.vue
+++ vue/src/components/Hedonometer.vue
@@ -12,9 +12,31 @@
       <div class="large-screens">
         <p>
           <select v-model="book" name="" id="">
-            <option value="Hedonometer/alice-in-wonderland-hedonometer.json">
-              Alice in Wonderland
-            </option>
+            <option value="Hedonometer/alice-in-wonderland-hedonometer.json">Alice in Wonderland</option>
+            <option value="Hedonometer/through-the-looking-glass-hedonometer.json">Through the Looking-Glass</option>
+            <option value="Hedonometer/wuthering-heights-hedonometer.json">Wuthering Heights</option>
+            <option value="Hedonometer/frankenstein-hedonometer.json">Frankenstein</option>
+            <option value="Hedonometer/pride-and-prejudice-hedonometer.json">Pride and Prejudice</option>
+            <option value="Hedonometer/importance-of-being-ernest-hedonometer.json">Importance of Being Ernest</option>
+            <option value="Hedonometer/moby-dick-hedonometer.json">Moby Dick</option>
+            <option value="Hedonometer/twenty-thousand-leagues-hedonometer.json">Twenty Thousand Leagues Under the Sea</option>
+            <option value="Hedonometer/tale-of-two-cities-hedonometer.json">Tale of Two Cities</option>
+            <option value="Hedonometer/picture-of-dorian-gray-hedonometer.json">Picture of Dorian Gray</option>
+            <option value="Hedonometer/great-gatsby-hedonometer.json">Great Gatsby</option>
+            <option value="Hedonometer/metamorphosis-hedonometer.json">Metamorphosis</option>
+            <option value="Hedonometer/dracula-hedonometer.json">Dracula</option>
+            <option value="Hedonometer/jane-eyre-hedonometer.json">Jane Eyre</option>
+            <option value="Hedonometer/heart-of-darkness-hedonometer.json">Heart of Darkness</option>
+            <option value="Hedonometer/huck-finn-hedonometer.json">The Adventures of Huckleberry Finn</option>
+            <option value="Hedonometer/secret-garden-hedonometer.json">The Secret Garden</option>
+            <option value="Hedonometer/war-of-the-worlds-hedonometer.json">The War of the Worlds</option>
+            <option value="Hedonometer/anna-karenina-hedonometer.json">Anna Karenina</option>
+            <option value="Hedonometer/uncle-tom-hedonometer.json">Uncle Tom's Cabin</option>
+            <option value="Hedonometer/little-women-hedonometer.json">Little Women</option>
+            <option value="Hedonometer/scarlet-letter-hedonometer.json">The Scarlet Letter</option>
+            <option value="Hedonometer/dr-jekyll-hedonometer.json">The Strange Case of Dr Jekyll and Mr Hyde</option>
+            <option value="Hedonometer/tom-sawyer-hedonometer.json">The Adventures of Tom Sawyer</option>
+            <option value="Hedonometer/sherlock-holmes-hedonometer.json">The Adventures of Sherlock Holmes</option>
           </select>
         </p>
         <Bubbles :filePath="book" />
